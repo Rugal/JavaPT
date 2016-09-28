140e9e321e6eafe6defb8b4ceb517345a629c92a
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2016-09-28 01:45:20 -0400

    test port

--- src/main/java/ga/rugal/jpt/springmvc/controller/ExceptionAction.java
+++ src/main/java/ga/rugal/jpt/springmvc/controller/ExceptionAction.java
@@ -3,6 +3,9 @@ package ga.rugal.jpt.springmvc.controller;
 import ml.rugal.sshcommon.springmvc.controller.BaseExceptionAction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
@@ -14,4 +17,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 public class ExceptionAction extends BaseExceptionAction
 {
 
+    @ResponseBody
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String test()
+    {
+        return "Cool";
+    }
 }
