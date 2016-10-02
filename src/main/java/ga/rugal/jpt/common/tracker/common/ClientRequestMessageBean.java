7fffe5fd849135eecefa2f0bf84fa7a50bd3f145
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2016-10-01 23:28:49 -0400

    Rename Jacoco folder; RequestBeanService UT, coverage; Use @Data

--- src/main/java/ga/rugal/jpt/common/tracker/common/ClientRequestMessageBean.java
+++ src/main/java/ga/rugal/jpt/common/tracker/common/ClientRequestMessageBean.java
@@ -2,16 +2,16 @@ package ga.rugal.jpt.common.tracker.common;
 
 import java.nio.ByteBuffer;
 import javax.validation.constraints.NotNull;
+import lombok.Data;
 
 /**
- * This is a JavaBean that are sent from client. Just one JavaBean includes all properties needed.
- * We will extract information from this bean to generate a formal tracker update bean.
- * <p>
- * Actually in 0.1 version, info_hash and peer_id fields need to be injected separately.
+ * This bean used to deserialize the information sent from client annonuc.<BR>
+ * Both info_hash and peer_id fields need to be injected separately.
  *
  * @author Rugal Bernstein
  * @since 0.1
  */
+@Data
 public class ClientRequestMessageBean
 {
 
@@ -54,39 +54,9 @@ public class ClientRequestMessageBean
     {
     }
 
-    public String getEvent()
-    {
-        return event;
-    }
-
-    public void setEvent(String event)
-    {
-        this.event = event;
-    }
-
-    public String getCredential()
-    {
-        return credential;
-    }
-
-    public void setCredential(String credential)
-    {
-        this.credential = credential;
-    }
-
     public ByteBuffer getBufferPeerId()
     {
         return ByteBuffer.wrap(peer_id.getBytes());
-    }
-
-    public String getInfoHash()
-    {
-        return info_hash;
-    }
-
-    public String getPeerId()
-    {
-        return peer_id;
     }
 
     public byte[] getByteInfoHash()
@@ -94,138 +64,8 @@ public class ClientRequestMessageBean
         return info_hash.getBytes();
     }
 
-    public void setInfo_hash(String info_hash)
-    {
-        this.info_hash = info_hash;
-    }
-
     public byte[] getBytePeerId()
     {
         return peer_id.getBytes();
-    }
-
-    public void setPeer_id(String peer_id)
-    {
-        this.peer_id = peer_id;
-    }
-
-    public int getPort()
-    {
-        return port;
-    }
-
-    public void setPort(int port)
-    {
-        this.port = port;
-    }
-
-    public long getDownloaded()
-    {
-        return downloaded;
-    }
-
-    public void setDownloaded(long downloaded)
-    {
-        this.downloaded = downloaded;
-    }
-
-    public long getUploaded()
-    {
-        return uploaded;
-    }
-
-    public void setUploaded(long uploaded)
-    {
-        this.uploaded = uploaded;
-    }
-
-    public long getLeft()
-    {
-        return left;
-    }
-
-    public void setLeft(long left)
-    {
-        this.left = left;
-    }
-
-    public int getCompact()
-    {
-        return compact;
-    }
-
-    public void setCompact(int compact)
-    {
-        this.compact = compact;
-    }
-
-    public int getNoPeerId()
-    {
-        return no_peer_id;
-    }
-
-    public void setNo_peer_id(int no_peer_id)
-    {
-        this.no_peer_id = no_peer_id;
-    }
-
-    public String getIp()
-    {
-        return ip;
-    }
-
-    public void setIp(String ip)
-    {
-        this.ip = ip;
-    }
-
-    public int getNumwant()
-    {
-        return numwant;
-    }
-
-    public void setNumwant(int numwant)
-    {
-        this.numwant = numwant;
-    }
-
-    public String getTrackerid()
-    {
-        return trackerid;
-    }
-
-    public void setTrackerid(String trackerid)
-    {
-        this.trackerid = trackerid;
-    }
-
-    public String getCorrupt()
-    {
-        return corrupt;
-    }
-
-    public void setCorrupt(String corrupt)
-    {
-        this.corrupt = corrupt;
-    }
-
-    public String getKey()
-    {
-        return key;
-    }
-
-    public void setKey(String key)
-    {
-        this.key = key;
-    }
-
-    public int getUid()
-    {
-        return uid;
-    }
-
-    public void setUid(int uid)
-    {
-        this.uid = uid;
     }
 }
